8affbbb795ec4a9c41655c39437093be62427d6f
Bdvoga <samib@mail.ru>
2020-12-06 23:47:03 +0200

    Урок 8 - задач 1 & 2

---
+++ src/sql/lesson8/CHANGE_TABLE_PRODUCTS.sql
@@ -0,0 +1,15 @@
+--Удаляет последние три колонки из таблицы Employees
+ALTER TABLE EMPLOYEES DROP COLUMN PHOTO;
+ALTER TABLE EMPLOYEES DROP COLUMN NOTES;
+ALTER TABLE EMPLOYEES DROP COLUMN REPORTS_TO;
+
+--Измененяет PK таблицы Suppliers с SupplierID на Phone
+ALTER TABLE PRODUCTS DROP CONSTRAINT SUPPLIERS_ID_FK;
+ALTER TABLE SUPPLIERS DROP CONSTRAINT SUPPLIERS_ID_PK;
+ALTER TABLE SUPPLIERS ADD CONSTRAINT PHONE_FK PRIMARY KEY (PHONE);
+
+--Добавляет в таблицу Customers колонку IS_ACTIVE, которая должна принимать одно из двух значений - 0 или 1 и не быть пустой
+ALTER TABLE CUSTOMERS ADD IS_ACTIVE NUMBER(1) CHECK (IS_ACTIVE IN(0,1)) NOT NULL;
+
+--Обновите длину поля Picture в таблице Categories до 100 символов
+ALTER TABLE CATEGORIES MODIFY PICTURE NVARCHAR2(100);
